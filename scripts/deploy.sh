84ecb8ee7c86b03afea58c593a013f95688c1fe7
Tizee <tizee@qq.com>
2019-03-19 14:21:59 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 git config --global user.name tizee-bot
 git config --global user.email tizee-bot@travis-ci.org
 
-git add --all
-git commit -m ":robot: generated readme doc"
-git push -q "https://${GITHUB_TOKEN}@githun.com/${TRAVIS_REPO_SLUG}.git" HEAD:master
+git add --all >/dev/null 2>&1
+git commit -m ":robot: generated readme doc" >/dev/null 2>&1
+git push -q "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" HEAD:master
 
